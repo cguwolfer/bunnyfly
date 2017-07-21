2c8dbf95130ece8431f519deae8e2c05ad8cb3e3
Minhsiang <minhsiang@gmail.com>
2017-07-21 10:28:49 +0800

    Add files via upload

---
+++ php_library/db_connection.php
@@ -0,0 +1,12 @@
+<?php 
+require_once "meekrodb.2.3.class.php";
+
+DB::$user = 'root';
+DB::$password = 'mysql';
+DB::$dbName = 'bunny';
+DB::$host = '127.0.0.1'; //defaults to localhost if omitted
+DB::$port = '3307'; // defaults to 3306 if omitted\
+DB::$encoding = 'utf8';
+DB::$error_handler = false; // since we're catching errors, don't need error handler
+DB::$throw_exception_on_error = true;
+?>
