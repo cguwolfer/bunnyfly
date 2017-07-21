d355335c2c7508b2325f0d3f70d7f933a720633d
Minhsiang <minhsiang@gmail.com>
2017-07-21 10:20:56 +0800

    finish login and register function

--- 5-0-1/js/register.js
+++ 5-0-1/js/register.js
@@ -16,9 +16,41 @@ function register() {
     var confirmEmail = document.getElementById("confirmEmail").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
-    if (email == confirmEmail && password == confirmPassword) {
+    var emailMsg = document.getElementById("emailMsg").value;
+    var name = document.getElementById("name").value;
+
+    if (email == confirmEmail && password == confirmPassword && !emailMsg) {
+        //儲存會員資訊()
+        if (typeof(Storage) !== "undefined") {
+            sessionStorage.clear();
+            
+            sessionStorage.setItem('email', email);
+            sessionStorage.setItem('nickName', name);
+            sessionStorage.setItem('password', password);
+        }        
         return true;
     } else {
         return false;
+    }
+}
+
+function checkMailExist(str) {
+    if (str.length == 0) {
+        document.getElementById("emailMsg").innerHTML = null;
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                if (this.responseText == "exist") {
+                    document.getElementById("emailMsg").innerHTML = "mail is used!";
+                } else {
+                    document.getElementById("emailMsg").innerHTML = null;
+                }
+                // console.log("checkMailExist:" + this.responseText);
+            }
+        }
+        xmlhttp.open("GET", "./php/checkMailExist.php?email=" + str, true);
+        xmlhttp.send();
     }
 }
